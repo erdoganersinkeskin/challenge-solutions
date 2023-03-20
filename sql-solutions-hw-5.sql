89a2a47d1184e3081280a042029b40ad81ec5527
erdoganersinkeskin <erdogannersinnkeskinn@gmail.com>
2023-03-20 16:21:38 +0300

    Create sql-solutions-hw-5.sql

---
+++ sql-solutions-hw-5.sql
@@ -0,0 +1,18 @@
+-- Answers for HW5 Questions.
+
+
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+OFFSET 5
+LIMIT 5;
+
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
